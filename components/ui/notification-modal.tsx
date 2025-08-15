130bf237b9bcae2705610c67eee97811b75fa78a
Aaron Jordan <aajordan10@gmail.com>
2025-08-15 13:50:45 -0400

    Remove toast dependency from notification modal

--- components/ui/notification-modal.tsx
+++ components/ui/notification-modal.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { X, CheckCircle, XCircle, AlertTriangle, Info } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import { X, CheckCircle, XCircle, AlertTriangle, Info, Clock } from 'lucide-react';
 import { Notification } from '@/types/notification';
 import { cn } from '@/lib/utils';
 
